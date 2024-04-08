f98c9e85538e0fab67d5d6914261f3c74ed84f3f
Vitaliy-Salinskiy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-04-08 19:26:28 +0200

    User logic implementation

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { prisma } from "@/lib/prisma";
+
+export const POST = async (req: NextRequest) => {
+  const { fingerprintId, email, name } = await req.json();
+
+  if (!fingerprintId || !email || !name) {
+    return NextResponse.json({ message: "Bad Request" }, { status: 400 });
+  }
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        fingerprintId,
+        email,
+        name,
+      },
+    });
+
+    if (!newUser) {
+      return NextResponse.json({ message: "Bad Request" }, { status: 400 });
+    }
+
+    return NextResponse.json({ newUser }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as Error).message },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async (_req: NextRequest) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as Error).message },
+      { status: 500 }
+    );
+  }
+};
